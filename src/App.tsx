06caafb8bab3d05af4cae4c3e61a96ca40f8ca0f
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-05-15 10:45:38 -0300

    Global style fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./Styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './Styles/themes/default'
+import { GlobalStyle } from './Styles/global'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme} >
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <h1>Hello world</h1>
     </ThemeProvider>
   )
