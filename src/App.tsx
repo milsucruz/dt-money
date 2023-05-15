a5281da63ebdb5c65254f03c926683312a65a9f4
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-05-15 10:05:44 -0300

    First styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./Styles/themes/default";
+
 export function App() {
   return (
+    <ThemeProvider theme={defaultTheme} >
       <h1>Hello world</h1>
+    </ThemeProvider>
   )
 }
-
-// font-family: 'Roboto', sans-serif;
