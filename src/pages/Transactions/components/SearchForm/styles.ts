443d42d3f5a4798c572a71606d121f07879a76d9
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-05-22 13:51:25 -0300

    SearchForm

--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -32,7 +32,12 @@ export const SearchFomrContainer = styled.form`
 
     cursor: pointer;
 
-    &:hover {
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover {
       background: ${(props) => props.theme['green-500']};
       border: 1px solid ${(props) => props.theme['green-500']};
       color: ${(props) => props.theme.white};
